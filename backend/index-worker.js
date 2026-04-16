f2bafe70120fd252fad64bbc7962402a12e6f104
Anderson Pedro <dingolandia@gmail.com>
2026-04-16 01:23:40 -0300

    debug(cpanel): loga falhas no entrypoint

--- backend/index-worker.js
+++ backend/index-worker.js
@@ -1,1 +1,10 @@
+process.on('uncaughtException', err => {
+    console.error('[worker uncaughtException]', err);
+});
+
+process.on('unhandledRejection', err => {
+    console.error('[worker unhandledRejection]', err);
+});
+
+console.log('[worker entry] loading dist/src/index-worker');
 require('./src/index-worker');
--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,10 @@
+process.on('uncaughtException', err => {
+    console.error('[backend uncaughtException]', err);
+});
+
+process.on('unhandledRejection', err => {
+    console.error('[backend unhandledRejection]', err);
+});
+
+console.log('[backend entry] loading dist/src/index');
 require('./src/index');
